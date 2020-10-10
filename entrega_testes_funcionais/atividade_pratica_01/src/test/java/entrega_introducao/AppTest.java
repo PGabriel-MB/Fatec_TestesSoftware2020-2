ebe5366e0b70055e7330375e3f65cb28d20b453e
PGabriel-MB <pablogrbarbosa@gmail.com>
2020-10-10 17:35:08 -0300

    testes implementados, editados e passando corretamente

--- entrega_testes_funcionais/atividade_pratica_01/src/test/java/entrega_introducao/AppTest.java
+++ entrega_testes_funcionais/atividade_pratica_01/src/test/java/entrega_introducao/AppTest.java
@@ -10,18 +10,21 @@ public class AppTest {
         o sistema consegue dizer se a data indicada cai em um 'Fim de Semana' ou
         se cai em algum 'Dia de Semana'
 
-        Sendo toda a criação dos testes se baseando nas técnicas de TDD
+        Sendo toda a criação dos testes se baseando nas técnicas de TDD.
+        Sendo assim temos duas Partições de Equivalência a serem testadas:
+            - Fim de Semana;
+            - Dia de Semana;
     */
 
     @Test
     public void fimDeSemanaTest() {
         DayOfWeek dow = new DayOfWeek();
-        assertEquals("domingo, Fim de Semana", dow.fimDeSemana(10, 9, 1998));
+        assertEquals("domingo, Fim de Semana", dow.fimDeSemana(11, 10, 2020));
     }
 
     @Test
     public void diaDeSemanaTest() {
         DayOfWeek dow = new DayOfWeek();
-        assertEquals("segunda-feira, Dia de Semana", dow.fimDeSemana(24, 2, 2003));
+        assertEquals("segunda-feira, Dia de Semana", dow.fimDeSemana(12, 10, 2020));
     }
 }
